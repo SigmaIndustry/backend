3c5d0a4d8d65ce254b011a799c81b6712aedc39e
Mykhailo Ovcharenko <nuredev@github.com>
2023-10-19 20:37:06 +0300

    Move enums into a separate file

---
+++ api/enums.py
@@ -0,0 +1,12 @@
+SEX = (
+    ("M", "Male"),
+    ("F", "Female"),
+)
+
+ROLES = (
+    ("A", "Admin"),
+    ("P", "Provider"),
+    ("G", "Guest"),
+)
+
+CATEGORIES = (("00", "Food"),)
--- api/models.py
+++ api/models.py
@@ -1,20 +1,6 @@
 from django.db import models
 
-
-GENDERS = (
-    ("M", "Male"),
-    ("F", "Female"),
-)
-
-ROLES = (
-    ("A", "Admin"),
-    ("P", "Provider"),
-    ("G", "Guest"),
-)
-
-CATEGORIES = (
-    ("00", "Food"),
-)
+from api.enums import CATEGORIES, SEX, ROLES
 
 
 class User(models.Model):
