aea3ece64069d96634db0c0837c27abc40fc6a62
Mykhailo Ovcharenko <nuredev@github.com>
2023-10-22 17:07:30 +0300

    [Vercel] Fix database

--- server/settings.py
+++ server/settings.py
@@ -84,11 +84,14 @@ DATABASES = {
     # }
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_HOST"),
+        "NAME": "verceldb",
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("POSTGRES_HOST"),
-        "PORT": 5432,
+        "PORT": "5432",
+        "OPTIONS": {
+            "sslmode": "require",
+        },
     }
 }
 
