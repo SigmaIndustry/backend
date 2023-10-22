2687e0aac7b0c8859d071d8b20a11ecb70b734ea
Mykhailo Ovcharenko <nuredev@github.com>
2023-10-22 13:26:55 +0300

    Add url names

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path("", index),
-    path("enum/sex", sex),
-    path("enum/roles", roles),
-    path("enum/categories", categories),
+    path("enum/sex", sex, name="Sex enums"),
+    path("enum/roles", roles, name="Role enums"),
+    path("enum/categories", categories, name="Category enums"),
 ]
--- security/urls.py
+++ security/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("register", register),
-    path("login", login),
-    path("authenticate", authenticate),
+    path("register", register, name="Register user"),
+    path("register_provider", register_provider, name="Register service provider"),
+    path("login", login, name="Login user"),
+    path("authenticate", authenticate, name="Authenticate user using token"),
 ]
