5f96b76d284bc517632e8e4d955bc6e5e9302418
Mykhailo Ovcharenko <nuredev@github.com>
2023-10-31 12:53:17 +0300

    Change to use page limits and offsets

--- api/views.py
+++ api/views.py
@@ -60,7 +60,8 @@ def search_service(request: WSGIRequest):
         request,
         require={
             "query": str,
-            "page": "?int",
+            "page_limit": "?int",
+            "page_offset": "?int",
             "min_price": "?int",
             "max_price": "?int",
             "category": "?str",
@@ -97,8 +98,15 @@ def search_service(request: WSGIRequest):
             lambda service: bool(len(service.reviews.all()) != 0)
             == bool(data.get("has_reviews"))
         )
-    page = int(data.get("page", 0)) * 10
+    page_limit = int(data.get("page_limit", 0))
+    page_offset = int(data.get("page_offset", 0))
 
     return Response(
-        {"size": len(matches.list), "results": [ServiceSerializer(service).data for service in matches.list[page:page+10]]}
+        {
+            "size": len(matches.list),
+            "results": [
+                ServiceSerializer(service).data
+                for service in matches.list[page_offset:page_offset+page_limit]
+            ],
+        }
     )
