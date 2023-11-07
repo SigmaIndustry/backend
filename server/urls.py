f0ee596e181cad45bcd01ef55b4b8d2d37c401b3
Mykhailo Ovcharenko <nuredev@github.com>
2023-11-07 12:20:29 +0200

    Add home page to prevent 404

--- server/urls.py
+++ server/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.conf import settings
+from django.http import JsonResponse
 from django.urls import include, path
 from django.conf.urls.static import static
 
@@ -7,4 +8,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("security/", include("security.urls")),
+    path("", lambda req: JsonResponse({})),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
