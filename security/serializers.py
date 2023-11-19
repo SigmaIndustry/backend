0d74d1295a55dc1b8eeeafb5de63656913a345c8
Mykhailo Ovcharenko <mykhailo.ovcharenko1@nure.ua>
2023-11-19 17:11:10 +0200

    make user return values instead of enums

--- security/serializers.py
+++ security/serializers.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
 
-from api.models import ServiceProvider
+from __enums__ import ROLES, SEX
 from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    sex = serializers.SerializerMethodField()
+    role = serializers.SerializerMethodField()
+
     class Meta:
         model = User
         fields = [
@@ -17,3 +20,17 @@ class UserSerializer(serializers.ModelSerializer):
             "role",
             "created_at",
         ]
+
+    @staticmethod
+    def get_sex(obj: User):
+        for key, value in SEX:
+            if key == obj.sex:
+                return value
+        return "Unknown"
+
+    @staticmethod
+    def get_role(obj: User):
+        for key, value in ROLES:
+            if key == obj.role:
+                return value
+        return "Unknown"
